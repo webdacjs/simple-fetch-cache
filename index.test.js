ef1f35aee9f997ec3595c753900ef25bde4c0778
webdac <webdac@gmail.com>
2018-12-29 15:35:57 +0100

    Fixing index tests

--- index.test.js
+++ index.test.js
@@ -3,12 +3,12 @@ const wikiSamplePage = 'https://www.mediawiki.org/w/api.php'
 
 test(`Testing ${wikiSamplePage} page is not cached on first fetch`, () => {
   fetch(wikiSamplePage, null, true).then(r => {
-    expect(get(r)).not.toBe(true)
+    expect(get(r.cached)).toBe(false)
   })
 })
 
 test(`Testing ${wikiSamplePage} page is cached on second fetch`, () => {
   fetch(wikiSamplePage, null, true).then(r => {
-    expect(get(r)).toBe(true)
+    expect(get(r.cached)).toBe(true)
   })
 })
