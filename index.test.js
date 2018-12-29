32e99bc4b583ad2690a50bef10fcbb98a3909bb2
webdac <webdac@gmail.com>
2018-12-29 16:00:27 +0100

    Clean up test

--- index.test.js
+++ index.test.js
@@ -2,13 +2,13 @@ const {fetch} = require('./index')
 const wikiSamplePage = 'https://www.mediawiki.org/w/api.php'
 
 test(`Testing ${wikiSamplePage} page is not cached on first fetch`, () => {
-  fetch(wikiSamplePage, null, true).then(r => {
+  fetch(wikiSamplePage).then(r => {
     expect(get(r.cached)).toBe(false)
   })
 })
 
 test(`Testing ${wikiSamplePage} page is cached on second fetch`, () => {
-  fetch(wikiSamplePage, null, true).then(r => {
+  fetch(wikiSamplePage).then(r => {
     expect(get(r.cached)).toBe(true)
   })
 })
